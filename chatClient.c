96620c9aed1166a2af68d324aca76f97150817b8
Nick Bonner <j.nick.bonner@gmail.com>
2017-02-14 14:59:27 -0700

    Added client. Can send single word messages to server and print out.

---
+++ chatClient.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+int main(){
+  int clientSocket;
+  socklen_t addr_size;
+
+  struct addrinfo hints, *res;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  getaddrinfo("127.0.0.1", "3635", &hints, &res);
+
+  clientSocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+
+  int status;
+  status = connect(clientSocket, res->ai_addr, res->ai_addrlen);
+  printf("%d\n\n", status);
+  if (status < 0){
+      return 0;
+  }
+  while (1){
+      char testBuffer[1024];
+      scanf("%s", testBuffer);
+      strcat(testBuffer, "\n");
+      send(clientSocket,testBuffer,13,0);
+      memset(&testBuffer[0], 0, sizeof(testBuffer));
+  }
+  close(clientSocket);
+
+
+  return 0;
+
+}
